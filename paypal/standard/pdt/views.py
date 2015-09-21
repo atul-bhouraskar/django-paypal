cd8e12f6818669a56e4ce4e031b5ad9a3ae4ee33
Alex Kasina <alexkasina1@gmail.com>
2015-09-21 04:24:40 -0400

    refactors in paypal/standard/pdt/views.py

--- paypal/standard/pdt/views.py
+++ paypal/standard/pdt/views.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.views.decorators.http import require_GET
 from paypal.standard.pdt.models import PayPalPDT
 from paypal.standard.pdt.forms import PayPalPDTForm
@@ -21,7 +21,7 @@ def pdt(request, item_check_callable=None, template="pdt/pdt.html", context=None
 
     context = context or {}
     context.update({"failed":failed, "pdt_obj":pdt_obj})
-    return render_to_response(template, context, RequestContext(request))
+    return render(request, template, context)
 
 
 def process_pdt(request, item_check_callable=None,
